cee76dcaa2216207d6ae3f328227eaa26f5b9fdb
David Escalona <davidkanc@gmail.com>
2015-05-24 23:53:12 +0200

    zombie ai controller

---
+++ Assets/scripts/ZombieAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        GetComponent<Animator>().SetFloat("WalkType", Random.value);
+        GetComponent<Animator>().SetFloat("Speed", 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
